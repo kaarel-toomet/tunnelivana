058fca46ca868ee6d2b524b4e7f2d34d50dc77ad
kaarel <kaarel.toomet@gmail.com>
2020-05-03 21:17:13 +0300

    monsters have gravity and they jump and move better and dont spawn underground

--- koll.gd
+++ koll.gd
@@ -8,6 +8,10 @@ signal hit
 signal unhit
 var hxy
 var killable
+
+var yvel = 0
+var onground
+
 export var speed = 3.5
 
 
@@ -23,8 +27,16 @@ func _process(delta):
 		return
 	hxy = get_parent().get_parent().get_node("hullmyts").position
 	var vel =  hxy-position
-	vel = vel.normalized()*speed
+	vel.y = 0
+	vel.x = sign(vel.x)
+	vel = vel*speed
 	move_and_slide(vel/delta)
+	move_and_slide(Vector2(0,yvel)/delta)
+	if not onground:
+		yvel += 1
+	else:
+		yvel = -15
+		onground = false
 	if Input.is_action_just_pressed("LCLICK") and killable:
 		get_parent().get_parent().get_node("hud").kolliv += 1
 		queue_free()
@@ -34,3 +46,11 @@ func _on_Area2D_mouse_entered():
 
 func _on_Area2D_mouse_exited():
 	killable = false
+
+
+func _on_groundboxx_body_entered(body):
+	onground = true
+
+
+func _on_groundboxx_body_exited(body):
+	onground = false
--- main.gd
+++ main.gd
@@ -18,7 +18,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("P"):
 		emit_signal("pause")
 		paused = !paused
-	if randi() % 750 == 0 and not paused:
+	if randi() % 200 == 0 and not paused:
 		var spawn = koll.instance()
 		$kollid.add_child(spawn)
 		var sxmax = $hullmyts.position.x+(1.5*$TileMap.chunkW*32)
@@ -27,5 +27,8 @@ func _process(delta):
 		var symax = $hullmyts.position.y+(1.5*$TileMap.chunkH*32)
 		var symin = $hullmyts.position.y-(1.5*$TileMap.chunkH*32)
 		var sy = (randi() % int((symax-symin)) + symin)
+		while $TileMap.get_cell(sx/32,sy/32) in $TileMap.solid:
+			sx = (randi() % int((sxmax-sxmin)) + sxmin)
+			sy = (randi() % int((symax-symin)) + symin)
 		spawn.position = Vector2(sx,sy)
 		spawn.scale = Vector2(2,2)
